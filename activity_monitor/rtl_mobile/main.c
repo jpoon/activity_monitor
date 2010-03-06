e60afa2bdf103e90992618dfd0968a36d83665d2
Jason-Linux <git@jasonpoon.ca>
2010-03-05 19:22:01 -0800

    Sending sensor data from mobile node

--- activity_monitor/rtl_mobile/main.c
+++ activity_monitor/rtl_mobile/main.c
@@ -6,6 +6,10 @@
 #include <hal.h>
 #include <rt_link.h>
 #include <nrk_error.h>
+#include <nrk_timer.h>
+#include <nrk_driver_list.h>
+#include <nrk_driver.h>
+#include <ff_basic_sensor.h>
 
 #define MY_TX_SLOT  8
 #define MY_RX_SLOT  0
@@ -15,6 +19,7 @@ nrk_task_type TaskOne;
 void tx_handler(void);
 
 void nrk_create_taskset();
+void nrk_register_drivers();
 
 // tx, rx buffers
 uint8_t tx_buf[MAX_RTL_PKT_SIZE];
@@ -27,7 +32,6 @@ int main(void)
 
     nrk_kprintf( PSTR("Starting up...\r\n") );
 
-	nrk_gpio_clr(NRK_DEBUG_0);
     nrk_init();
 
     nrk_led_clr(ORANGE_LED);
@@ -37,6 +41,7 @@ int main(void)
 
     nrk_time_set(0,0);
     rtl_task_config();
+    nrk_register_drivers();
     nrk_create_taskset();
     nrk_start();
   
@@ -45,14 +50,16 @@ int main(void)
 
 void tx_handler()
 {
+    uint8_t fd, val;
     uint8_t *local_rx_buf;
     uint8_t cnt = 0;
     uint8_t length;
+    uint8_t adxl_x, adxl_y, adxl_z, mic, light, temp, bat;
 
     printf( "TX: pid %d\r\n", nrk_get_pid());
 
-    rtl_init(RTL_MOBILE);
     nrk_kprintf( PSTR("RT-Link: RTL_MOBILE\r\n") );
+    rtl_init(RTL_MOBILE);
     rtl_set_channel(15);
     rtl_set_schedule( RTL_TX, MY_TX_SLOT, 1 ); 
     rtl_set_schedule( RTL_RX, MY_RX_SLOT, 1 ); 
@@ -89,12 +96,37 @@ void tx_handler()
             nrk_kprintf( PSTR("TX: pending packet on slot ") );
             printf( "%d\r\n", MY_TX_SLOT );
         } else {
+            // Open ADC device as read 
+            fd = nrk_open(FIREFLY_SENSOR_BASIC,READ);
+            if( fd==NRK_ERROR ) {
+                nrk_kprintf( PSTR("Sensors: Failed to open sensor driver\r\n"));
+            }
+
+            val=nrk_set_status(fd,SENSOR_SELECT,BAT);
+            val=nrk_read(fd,&bat,2);
+            val=nrk_set_status(fd,SENSOR_SELECT,LIGHT);
+            val=nrk_read(fd,&light,2);
+            val=nrk_set_status(fd,SENSOR_SELECT,TEMP);
+            val=nrk_read(fd,&temp,2);
+            val=nrk_set_status(fd,SENSOR_SELECT,AUDIO);
+            val=nrk_read(fd,&mic,2);
+            val=nrk_set_status(fd,SENSOR_SELECT,ACC_X);
+            val=nrk_read(fd,&adxl_x,2);
+            val=nrk_set_status(fd,SENSOR_SELECT,ACC_Y);
+            val=nrk_read(fd,&adxl_y,2);
+            val=nrk_set_status(fd,SENSOR_SELECT,ACC_Z);
+            val=nrk_read(fd,&adxl_z,2);
+
+            nrk_close(fd);
+
+            // Build a sensor packet
+            sprintf (tx_buf, "bat=%d light=%d temp=%d accel=%d %d %d %d\r\n", bat, light, temp, mic, adxl_x, adxl_y, adxl_z);
+ 
             nrk_kprintf( PSTR("TX: sending packet on slot "));
             printf( "%d\r\n", MY_TX_SLOT );
 
             nrk_led_set(GREEN_LED);
-            sprintf( &tx_buf[PKT_DATA_START], "Hello World %d", cnt++ ); 
-            length = strlen(&tx_buf[PKT_DATA_START])+PKT_DATA_START;
+            length = strlen(tx_buf);
             rtl_tx_pkt( tx_buf, length, MY_TX_SLOT );
             rtl_wait_until_rx_or_tx();
             nrk_led_clr(GREEN_LED);
@@ -109,16 +141,15 @@ void nrk_create_taskset()
     nrk_kprintf ( PSTR("Creating Taskset: ") );
 
     TaskOne.task = tx_handler;
-    TaskOne.Ptos = (void *) &Stack1[NRK_APP_STACKSIZE-1];
-    TaskOne.Pbos = (void *) &Stack1[0];
-    TaskOne.prio = 2;
+    nrk_task_set_stk( &TaskOne, Stack1, NRK_APP_STACKSIZE);
+    TaskOne.prio = 1;
     TaskOne.FirstActivation = TRUE;
     TaskOne.Type = BASIC_TASK;
     TaskOne.SchType = PREEMPTIVE;
-    TaskOne.period.secs = 1;
-    TaskOne.period.nano_secs = 0;
-    TaskOne.cpu_reserve.secs = 0;
-    TaskOne.cpu_reserve.nano_secs = 100*NANOS_PER_MS;
+    TaskOne.period.secs = 5;
+    TaskOne.period.nano_secs = 100*NANOS_PER_MS;
+    TaskOne.cpu_reserve.secs = 1;
+    TaskOne.cpu_reserve.nano_secs = 50*NANOS_PER_MS;
     TaskOne.offset.secs = 0;
     TaskOne.offset.nano_secs= 0;
     nrk_activate_task (&TaskOne);
@@ -126,4 +157,21 @@ void nrk_create_taskset()
     nrk_kprintf ( PSTR("Done\r\n") );
 }
 
+void nrk_register_drivers()
+{
+    int8_t val;
 
+// Register the Basic FireFly Sensor device driver
+// Make sure to add: 
+//     #define NRK_MAX_DRIVER_CNT  
+//     in nrk_cfg.h
+// Make sure to add: 
+//     SRC += $(ROOT_DIR)/src/drivers/platform/$(PLATFORM_TYPE)/source/ff_basic_sensor.c
+//     in makefile
+    val = nrk_register_driver( &dev_manager_ff_sensors,FIREFLY_SENSOR_BASIC);
+    if( val==NRK_ERROR ) {
+        nrk_kprintf( PSTR("Drivers: Failed to load ADC driver\r\n") );
+    } else {
+        nrk_kprintf( PSTR("Drivers: Loaded Successfully\r\n") );
+    }
+}

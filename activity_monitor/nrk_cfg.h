6039c2178dd2bdc85c718ad6af9ffec3ac21fa74
Jason-Linux <git@jasonpoon.ca>
2010-03-09 18:37:48 -0800

    cleaning the nrk cfg file. defining stat tracker

--- activity_monitor/nrk_cfg.h
+++ activity_monitor/nrk_cfg.h
@@ -4,31 +4,19 @@
 #ifndef __nrk_cfg_h	
 #define __nrk_cfg_h
 
-// NRK_REPORT_ERRORS will cause the kernel to print out information about
-// missed deadlines or reserve violations
 #define NRK_REPORT_ERRORS
 
-// NRK_HALT_ON_ERRORS will cause the kernel to freeze on errors so that
-// it is easier to see debugging messages.
 #define NRK_HALT_AND_LOOP_ON_ERROR
-
-// If you want to disable the looping kernel error message when the system
-// halts, then include the following define.  This will automatically
-// halt the error as well.
 //#define NRK_HALT_ON_ERROR
 
 #define IGNORE_BROWN_OUT_ERROR
 #define IGNORE_EXT_RST_ERROR
 
-// NRK_STACK_CHECK adds a little check to see if the bottom of the stack
-// has been over written on all suspend calls
+#define NRK_STATS_TRACKER
+
 #define NRK_STACK_CHECK
 
-// Max number of tasks in your application
-// Be sure to include the idle task
-// Making this the correct size will save on BSS memory which
-// is both RAM and ROM...
-#define NRK_MAX_TASKS                   5
+#define NRK_MAX_TASKS                   4
 
 // NRK_MAX_RESOURCE_CNT defines the number of semaphores in the system.
 // If you don't use any semaphores, set this to 0.  Be sure that libraries

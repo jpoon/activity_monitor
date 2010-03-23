723ff9cb68dc422cfc4b963809745f9163a4a232
Jason-Linux <git@jasonpoon.ca>
2010-03-23 02:34:55 -0700

    adding grid to graph

--- slipstream/client/graph/line_graph.py
+++ slipstream/client/graph/line_graph.py
@@ -10,8 +10,9 @@ class LineGraph:
     def create(self, name, data, v_bounds = None):
         CairoPlot.dot_line_plot(name=name,
                                 data=data,
-                                width=1000,
-                                height=1000,
+                                width=500,
+                                height=500,
+                                grid=True,
                                 v_bounds=v_bounds,
                                 axis=True)
 

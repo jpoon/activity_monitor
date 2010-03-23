adf0629bc3755c514443033df5827533e8147b23
Jason-Linux <git@jasonpoon.ca>
2010-03-22 23:57:31 -0700

    ability to add v_bounds to graph

--- slipstream/client/graph/line_graph.py
+++ slipstream/client/graph/line_graph.py
@@ -7,10 +7,11 @@ class LineGraph:
     def __init__(self):
         pass
 
-    def create(self, name, data):
+    def create(self, name, data, v_bounds = None):
         CairoPlot.dot_line_plot(name=name,
                                 data=data,
                                 width=1000,
                                 height=1000,
+                                v_bounds=v_bounds,
                                 axis=True)
 

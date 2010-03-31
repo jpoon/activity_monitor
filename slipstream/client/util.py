c0ff7536184f2bd7cb4cda5a359fd550da188758
Jason-Linux <git@jasonpoon.ca>
2010-03-30 22:29:48 -0700

    adding watcher class obtained from activestate recipes. class watches for sigint

--- slipstream/client/util.py
+++ slipstream/client/util.py
@@ -1,4 +1,5 @@
 from threading import *
+import os, signal, sys, operator
 
 class StoppableThread(Thread):
     """Thread class with a stop() method. The thread itself has to check
@@ -13,4 +14,48 @@ class StoppableThread(Thread):
 
     def stopped(self):
         return self._stop.isSet()
+
+class Watcher:
+    """this class solves two problems with multithreaded
+    programs in Python, (1) a signal might be delivered
+    to any thread (which is just a malfeature) and (2) if
+    the thread that gets the signal is waiting, the signal
+    is ignored (which is a bug).
+
+    The watcher is a concurrent process (not thread) that
+    waits for a signal and the process that contains the
+    threads.  See Appendix A of The Little Book of Semaphores.
+    http://greenteapress.com/semaphores/
+
+    I have only tested this on Linux.  I would expect it to
+    work on the Macintosh and not work on Windows.
+
+    http://code.activestate.com/recipes/496735-workaround-for-missed-sigint-in-multithreaded-prog/
+    """
+    
+    def __init__(self):
+        """ Creates a child thread, which returns.  The parent
+            thread waits for a KeyboardInterrupt and then kills
+            the child thread.
+        """
+        self.child = os.fork()
+        if self.child == 0:
+            return
+        else:
+            self.watch()
+
+    def watch(self):
+        try:
+            os.wait()
+        except KeyboardInterrupt:
+            # I put the capital B in KeyBoardInterrupt so I can
+            # tell when the Watcher gets the SIGINT
+            print 'KeyBoardInterrupt'
+            self.kill()
+        sys.exit()
+
+    def kill(self):
+        try:
+            os.kill(self.child, signal.SIGKILL)
+        except OSError: pass
 

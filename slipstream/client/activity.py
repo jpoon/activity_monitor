e4196e931826196331ac8a8cee7e826e27fdae8f
Jason-Linux <git@jasonpoon.ca>
2010-04-11 11:35:15 -0700

    instead of checking for each scenario, follow a decision tree method (e.g. lots of if, elses)

--- slipstream/client/activity.py
+++ slipstream/client/activity.py
@@ -43,6 +43,7 @@ class Activity:
 
     def __init__(self, keys):
         self.logging = logging.getLogger("activity")
+        self.history = None
 
         self._sensorDict = {}
         for k in keys:
@@ -52,77 +53,73 @@ class Activity:
         self._sensorDict[name].add(avg, std_deviation)
 
     def doAllTests(self):
+        legs = ["left_leg", "right_leg"]
+        arms = ["left_arm", "right_arm"]
+
         for k in self._sensorDict.keys():
             if self._sensorDict[k].getNumDataPoints() <= 0:
                 return
 
-        isStationary = True
-        for k in ["left_leg", "right_leg"]:
+        legs_stationary = True
+        for k in legs:
             std_dev = self._sensorDict[k].getStdDeviation()
 
             if not self.__isStable(std_dev):
-                isStationary = False
+                legs_stationary = False
                 break
 
-        if isStationary:
-            self.logging.info("Lying Down:\t%s", self.isLyingDown())
-            self.logging.info("Standing:\t\t %s", self.isStanding())
-            self.logging.info("Sitting:\t\t %s", self.isSitting())
-        else:
-            self.logging.info("legs are moving")
-
-    def isLyingDown(self):
-        for k in self._sensorDict.keys():
-            avg = self._sensorDict[k].getAverage()
-
-            if not self.__isHorizontal(avg):
-                return False
- 
-        return True
-
-    def isSitting(self):
-        for k in self._sensorDict.keys():
-            avg = self._sensorDict[k].getAverage()
-
-            if k in ["left_arm", "right_arm"]:
-                if not self.__isVertical(avg):
-                    return False
+        if legs_stationary:
+            # stationary: sitting, lying down, standing
+            if self.isHorizontal(legs):
+                if self.isHorizontal(arms):
+                    # lying down
+                    if self.history is not "lying_down":
+                        print "Lying Down"
+                        self.history = "lying_down"
+                else:
+                    # sitting
+                    if self.history is not "sitting":
+                        print "Sitting"
+                        self.history = "sitting"
             else:
-                if not self.__isHorizontal(avg):
-                    return False
+                # standing
+                if self.history is not "standing":
+                    print "Standing"
+                    self.history = "standing"
+        else:
+            # moving: running, walking
+            print "moving.."
 
-        return True
+    def isHorizontal(self, list):
+        def horizontal((x, y, z)):
+            errorMargin = 0.20
 
-
-    def isStanding(self):
-        for k in self._sensorDict.keys():
-            avg = self._sensorDict[k].getAverage()
-
-            if not self.__isVertical(avg):
-                return False
+            if (-errorMargin) <= x <= (errorMargin):
+                if (-errorMargin) <= x <= (errorMargin):
+                        return True
+            return False
  
+        for k in list:
+            avg = self._sensorDict[k].getAverage()
+            if not horizontal(avg):
+                return False
         return True
 
-    def __isVertical(self, (x, y, z)):
-        errorMargin = 0.20
+    def isVertical(self, list):
+        def vertical((x, y, z)):
+            errorMargin = 0.20
 
-        if (1-errorMargin) <= x <= (1+errorMargin):
             if (-errorMargin) <= y <= (errorMargin):
                 if (-errorMargin) <= z <= (errorMargin):
                     return True
+            return False
 
-        return False
+        for k in list:
+            avg = self._sensorDict[k].getAverage()
+            if not vertical(avg):
+                return False
+        return True
 
-    def __isHorizontal(self, (x, y, z)):
-        errorMargin = 0.20
-
-        if (-errorMargin) <= x <= (errorMargin):
-            if (-errorMargin) <= y <= (errorMargin):
-                if (1-errorMargin) <= z <= (1+errorMargin):
-                    return True
-
-        return False
-        
     def __isStable(self, (x, y, z)):
         errorMargin = 0.15
 
